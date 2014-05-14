6ddd488b5d47b3768251c4b4b7c6c344368bf770
cihan emre <cekisakurek@gmail.com>
2014-05-14 18:52:00 +0200

    init

---
+++ CEKBundleCreatorCLI/Objects/CEKImage.h
@@ -0,0 +1,13 @@
+//
+//  CEKImage.h
+//  CEKBundleCreatorCLI
+//
+//  Created by Cihan Emre Kisakurek on 14/05/14.
+//  Copyright (c) 2014 Cihan Emre Kisakurek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CEKImage : NSObject
+
+@end
---
+++ CEKBundleCreatorCLI/Objects/CEKImage.m
@@ -0,0 +1,13 @@
+//
+//  CEKImage.m
+//  CEKBundleCreatorCLI
+//
+//  Created by Cihan Emre Kisakurek on 14/05/14.
+//  Copyright (c) 2014 Cihan Emre Kisakurek. All rights reserved.
+//
+
+#import "CEKImage.h"
+
+@implementation CEKImage
+
+@end
