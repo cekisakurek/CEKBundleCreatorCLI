6112bade2243b47a09502a400185614e50137463
cihan emre <cekisakurek@gmail.com>
2014-05-14 18:30:11 +0200

    Initial Commit

---
+++ CEKBundleCreatorCLI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CEKBundleCreatorCLI
+//
+//  Created by Cihan Emre Kisakurek on 14/05/14.
+//  Copyright (c) 2014 Cihan Emre Kisakurek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
